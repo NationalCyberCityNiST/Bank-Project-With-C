3073008bf5cba9e7043a493ee3b7973b4899a85a
NationalCyberCityNiST <136290937+NationalCyberCityNiST@users.noreply.github.com>
2023-07-02 18:17:19 +0630

    lesson 13

---
+++ lesson 13/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "onlinebank.h"
+
+int main(){
+    space_counter();
+    loading_from_file();
+    printing_all_data();
+    main_menu();
+    // get_time();
+    return 0;
+}
